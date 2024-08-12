b7a1e7f49c75ab0be4d53816b6a80dd218f6c3f0
Ángel Aguero <angelagueror23@gmail.com>
2024-08-12 00:27:17 -0400

    feat: Implementación de vistas y funcionalidades para editar Customer y Category
    - Añadidas las vistas para editar la entidad Customer y Category.
    - Implementadas las funcionalidades de edición, incluyendo validaciones y actualización en la base de datos.
    - Mejoras en la UI para facilitar la experiencia de usuario durante la edición.

--- src/main/java/com/github/angel/controller/CategoryController.java
+++ src/main/java/com/github/angel/controller/CategoryController.java
@@ -25,7 +25,6 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestBody;
 
 /**
  *
