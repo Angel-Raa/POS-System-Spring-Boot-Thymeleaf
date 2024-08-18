2b797634ad4a143939188f1e32f6fcd8973da207
Ángel Aguero <angelagueror23@gmail.com>
2024-08-18 00:10:15 -0400

    feat: Update purchase logic and improve application forms
    - Implemented logic for creating purchases in PurchaseService, including stock validation and total price calculation.
    - Removed `totalPrice` field from the purchase form since it is now calculated in the backend.
    - Added endpoint in CustomerController for searching customers by name.
    - Created logic in CustomerService to filter customers by name using a "LIKE" query.
    - Modified purchase form in Thymeleaf to include select fields for products and payment methods.
    - Added autocomplete field for selecting customers in the purchase form.
    - Updated the form to use Select instead of Input for Product and PaymentMethod.

--- src/main/java/com/github/angel/entity/Purchase.java
+++ src/main/java/com/github/angel/entity/Purchase.java
@@ -29,7 +29,6 @@ import jakarta.validation.constraints.DecimalMin;
 import jakarta.validation.constraints.Digits;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
-import jakarta.validation.constraints.PastOrPresent;
 import jakarta.validation.constraints.PositiveOrZero;
 import jakarta.validation.constraints.Size;
 
